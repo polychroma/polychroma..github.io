c5f23f63eff23e0f37c9a16dc0b9a25667299649
polychroma <polychromax@gmail.com>
2018-01-19 20:55:05 -0500

    Added a night mode button

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-
+function toggleNightMode() {
+  var color = document.getElementById('main-container').style.color;
+    
+  var backgroundColor = document.body.style.backgroundColor;
+  
+    
+  if (color == "black" && backgroundColor == "white") {
+        document.getElementById('main-container').style.color="white";
+        document.body.style.backgroundColor="black";
+        document.getElementById('main-container').style.fontFamily="Arial";
+        
+    } else {
+        document.getElementById('main-container').style.color="black";
+        document.body.style.backgroundColor="white";
+        document.getElementById('main-container').style.fontFamily="Times New Roman";
+    }
+}
